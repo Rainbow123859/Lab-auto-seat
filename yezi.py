5d78d3eacf716089b11f2f5d284f7675db0d33e6
Rainbow123859 <942451647@qq.com>
2025-11-11 13:19:09 +0800

    更新 yezi.py

--- yezi.py
+++ yezi.py
@@ -24,7 +24,7 @@ CONFIG = {
       #  {"seatno": "HNND20480", "seatname": "480", "datetime": "510,1320"},
        # {"seatno": "HNND20482", "seatname": "482", "datetime": "510,1320"},
         #{"seatno": "HNND20481", "seatname": "481", "datetime": "510,1320"},
-        {"seatno": "HNND04286", "seatname": "286", "datetime": "450,480"},
+        {"seatno": "HNND04292", "seatname": "292", "datetime": "480,1320"},
     ],
     "request_timeout": 5,
     "max_attempts": 10  # 每个座位的最大尝试次数
@@ -102,8 +102,8 @@ if __name__ == "__main__":
     booker = LibraryBooker(CONFIG)
     logger.info("程序已启动，等待预约时间窗口...")
 
-    start_time = dt_time(1, 59, 0)
-    end_time = dt_time(22, 8, 0)
+    start_time = dt_time(21, 59, 0)
+    end_time = dt_time(22, 4, 0)
 
     while True:
         now = datetime.now()

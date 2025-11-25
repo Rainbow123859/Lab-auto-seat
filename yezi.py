b758d37e61d00af39603956b911682b4ed65105e
Rainbow123859 <942451647@qq.com>
2025-11-25 10:09:47 +0800

    更新 yezi.py

--- yezi.py
+++ yezi.py
@@ -9,11 +9,11 @@ from datetime import datetime, time as dt_time
 # ============== 配置区域（根据你的抓包数据修改）===============
 BASE_CONFIG = {
     "cookies": {
-        "ASP.NET_SessionId": "qljak1tvqi2xve4cg50gi4ev",
+        "ASP.NET_SessionId": "kpl1bqr3ik0g4ssiavu3becj",
         "cookie_unit_name": "%e6%b9%96%e5%8d%97%e5%86%9c%e4%b8%9a%e5%a4%a7%e5%ad%a6%e5%9b%be%e4%b9%a6%e9%a6%86",
         "cookie_come_app": "D935AE54952F16C1",
-        "cookie_come_timestamp": "1762756864",
-        "cookie_come_sno": "DAD084FF07CB0C55B865F4CC47A8D55BBE7AF5FCE39EA877",
+        "cookie_come_timestamp": "1763036561",
+        "cookie_come_sno": "DAD084FF07CB0C550A5B0395536E594629DC32A157CF58BF",
         "dt_cookie_user_name_remember": "6C72C7227D4D5EEFBEEBC75F707B38B08AAABBE15EF81E84"
     },
     "latitude": "28.186214447021484",
